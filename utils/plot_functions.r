6d9a112c5aa1b378f9a1c0362eae5ab5fca62015
jackagoldman <jandrewgoldman@gmail.com>
2026-01-23 08:59:22 -0500

    add distributed lag analysis with updated workflwow with models for just median and extreme, as well LRT testing H3/4. Updated EDA. Figures updated to match new workflow. New vpd datasets, updated data wrangling and cleaning to reflect more parsimonious workflow. New model diagnostics and validation steps. Updated reporting to reflect new analyses and results.

--- utils/plot_functions.r
+++ utils/plot_functions.r
@@ -45,14 +45,13 @@ mask_too_far <- function(model, x, y, n = 100, dist = 0.05) {
 #' @param n, number of points to evaluate the smooth over
 #' @return a list with two dataframes: smooth_df (all estimates) and mask (significant estimates)
 
-get_lag_estimates <- function(model, lag_var = "L", smooth_var = "vpd", weights = "weights_btl", n = 100) {
+get_lag_estimates <- function(model, lag_var = "L", smooth_var = "vpd", n = 100) {
   
   if (!is.character(lag_var)) stop("lag_var must be a string, the name of the lag variable in the model")
   if (!is.character(smooth_var)) stop("smooth_var must be a string, the name of the smooth variable in the model")
-  if (!is.character(weights)) stop("weights must be a string, the name of the weights variable in the model")
   
   # Create the smooth specification
-  smooth = paste0("te(", smooth_var, ",", lag_var,"):",  weights)
+  smooth = paste0("te(", smooth_var, ",", lag_var,")")
 
 
   tf <- mask_too_far(model = model, x = lag_var, y = smooth_var, n = 100, dist = 0.1)
@@ -73,7 +72,7 @@ get_lag_estimates <- function(model, lag_var = "L", smooth_var = "vpd", weights 
 # Flag significant locations where CI excludes 0 (null effect)
     tibble::add_column(tf) |> 
     #dplyr::mutate(across(c(.estimate, .lower_ci, .upper_ci), ~ if_else(tf, NA_real_, .))) |>
-    dplyr::mutate(sig = ifelse(.lower_ci > 0 | .upper_ci < 0, TRUE, FALSE)) 
+    dplyr::mutate(sig = ifelse(.lower_ci > 0 | .upper_ci < 0 , TRUE, FALSE)) 
 
 return(smooth_df)
   }
@@ -88,11 +87,10 @@ return(smooth_df)
 plot_lag_smooth <- function(model, 
   smooth_df, 
   smooth_var, 
-  lag_var, 
-  weights) {
+  lag_var) {
   ylabel <- switch(smooth_var,
-                   vpd = "Vapour Pressure Deficit (z-score)",
-                   cmi = "Climate Moisture Index (z-score)",
+                   vpd = "Vapour Pressure Deficit",
+                   cmi = "Climate Moisture Index",
                    smooth_var)
   response <- as.character(formula(model)[[2]])
   response_lab <- if (response == "median") "Median\nBurn Severity" else if (response == "pct_95") "Extreme\nBurn Severity" else response
@@ -158,25 +156,6 @@ plot_lag_smooth <- function(model,
   p
 }
 
-###
-
-
-#' place label inside ggplot at specified position
-#' @param plot, ggplot object to modify
-#' @param label, text label to place
-#' @param x_pos, x position for label (default -Inf for left side)
-#' @param y_pos, y position for label (default 100)
-#' @param hjust, horizontal justification (default -0.5 to nudge left
-#' 
-place_label_inside <- function(plot, label, x_pos = -Inf, y_pos = 100, hjust = -0.5, vjust = 1, size = 5, padding = unit(5, "pt")) {
-  plot +
-    annotate("text",
-             label = label, x = x_pos, y = y_pos,
-             hjust = hjust, vjust = vjust, size = size,
-             gp = gpar(fontsize = size) # Use grid::gpar for more control if needed
-    ) +
-    theme(plot.margin = unit(c(0, 0, 0, 0), "pt")) # Optional: remove margins
-}
 
 
 #' get min max estimates
@@ -199,3 +178,148 @@ get_steepest <- function(mod.estimates){
 
   return(list(max = max_est, min = min_est, sig = sig_est))
 }
+
+
+
+
+#' draws function 
+#' @param model, a fitted GAM model
+#' @param smooth_id, index of the tensor smooth in the model
+#' @param q, quantile to fix the other variable at
+#' @param moisture_data, dataframe with moisture data to get quantiles and medians
+#' @param seed, random seed for reproducibility
+#' @return a list with curves dataframe, median dataframe, smooth variable name, and plot
+#' 
+#' Example usage: 
+#' test <- one_d_slice_draws(model = m_median, smooth_id = 1, q = "5%", moisture_data = moisture_data)
+#' p <- test$plot # get plot
+#' p # display plot
+#' curves_df <- test$curves # get curves dataframe
+#' median_df <- test$median # get median dataframe
+
+one_d_slice_draws <- function(model, smooth_id = 1, q = "5%" , moisture_data, seed = 42) {
+    # choose model (m or m1)
+    model <- model      # replace with m or m1 as appropriate
+    smooth_id <- smooth_id     # index of the tensor smooth (check by names(model$smooth))
+  
+  get_smooth_cols <- function(mod, smooth_id = 1) {
+    s <- mod$smooth[[smooth_id]] 
+    seq(s$first.para, s$last.para)
+  }
+
+  
+
+    if (smooth_id == 1){
+
+    # build grid/newdata for a particular CMI quartile (example: 50%)
+    quartiles <- quantile(moisture_data$cmi_wy, probs = c(0.05,0.25,0.5,0.75), na.rm = TRUE)
+
+    # Sequence for lags (LL)
+    ll_seq <- seq(min(moisture_data$LL, na.rm = TRUE), max(moisture_data$LL, na.rm = TRUE), length.out = 100)
+
+    # Fix other covariates at medians
+    pct_conf_med <- median(moisture_data$pct_conf, na.rm = TRUE)
+    dc_90th_med <- median(moisture_data$dc_90th, na.rm = TRUE)
+    vpd_med <- median(moisture_data$vpd, na.rm = TRUE)
+    l_seq <- min(moisture_data$L, na.rm = TRUE)
+    fire_size_class <- "large"
+    ecoregion_lvl <- 'LN'  # Representative ecoregion
+
+
+    newdat <- data.frame(
+      cmi_wy = rep(quartiles[q], length(ll_seq)),
+      LL = ll_seq,
+      pct_conf = pct_conf_med,
+      dc_90th = dc_90th_med,
+      fire_size_class = fire_size_class,
+      vpd = vpd_med,
+      L = l_seq,
+      ecoregion_lvl = ecoregion_lvl
+    )
+  }else {
+    # build grid/newdata for a particular CMI quartile (example: 50%)
+    quartiles <- quantile(moisture_data$vpd, probs = c(0.05,0.25,0.5,0.75), na.rm = TRUE)
+
+    # Sequence for lags (LL)
+    l_seq <- seq(min(moisture_data$L, na.rm = TRUE), max(moisture_data$L, na.rm = TRUE), length.out = 100)
+
+    # Fix other covariates at medians
+    pct_conf_med <- median(moisture_data$pct_conf, na.rm = TRUE)
+    dc_90th_med <- median(moisture_data$dc_90th, na.rm = TRUE)
+    cmi_wy_med <- median(moisture_data$cmi_wy, na.rm = TRUE)
+    ll_seq <- min(moisture_data$LL, na.rm = TRUE)
+    fire_size_class <- "large"
+    ecoregion_lvl <- 'LN'  # Representative ecoregion
+
+
+    newdat <- data.frame(
+      vpd = rep(quartiles[q], length(l_seq)),
+      LL = ll_seq,
+      pct_conf = pct_conf_med,
+      dc_90th = dc_90th_med,
+      fire_size_class = fire_size_class,
+      cmi_wy = cmi_wy_med,
+      L = l_seq,
+      ecoregion_lvl = ecoregion_lvl
+    )
+  }
+
+    B <- 200
+    beta_hat <- coef(model)
+    Vb <- vcov(model)
+    df_t <- 1000   # choose degrees of freedom
+
+    # get Xp_partial (one-time)
+    Xp <- predict(model, newdata = newdat, type = "lpmatrix")
+    cols <- get_smooth_cols(model, smooth_id)
+    Xp_part <- matrix(0, nrow = nrow(Xp), ncol = ncol(Xp))
+    Xp_part[, cols] <- Xp[, cols, drop = FALSE]
+
+    # simulate B coefficient draws from multivariate-t and compute curves
+    set.seed(seed)
+    beta_draws <- mvtnorm::rmvt(B, sigma = Vb, df = df_t, delta = beta_hat, type = "shifted")
+    # beta_draws: B x p, convert to p x B if needed
+    curve_mat <- t(apply(beta_draws, 1, function(b) as.numeric(Xp_part %*% b))) # B x ngrid
+    curve_mat <- t(curve_mat)   # ngrid x B
+
+
+    B_plot <- min(50, ncol(curve_mat))
+    cols_to_plot <- sample(ncol(curve_mat), B_plot)
+
+    if (smooth_id == 1){
+    smooth_var <- "cmi_wy"
+    df_long <- as_tibble(curve_mat[, cols_to_plot]) %>%
+      mutate(LL = newdat$LL) %>%
+      pivot_longer(-LL, names_to = "sim", values_to = "partial")
+
+    # median curve across refits
+    median_curve <- apply(curve_mat, 1, mean, na.rm = TRUE)
+    median_df <- tibble(LL = newdat$LL, fit = median_curve,  quartile = q)
+     plot <- ggplot(df_long, aes(x = LL, y = partial, group = sim)) +
+        geom_line(alpha = 0.12, color = "black") +
+        geom_line(data = median_df, aes(x = LL, y = fit), color = "firebrick", linewidth = 1.1, inherit.aes = FALSE) +
+        scale_x_reverse() +
+        labs(x = "Lag", y = "Partial effect", title = "Refit subsample curves + median") +
+        theme_bw()+ geom_hline(yintercept = 0, linetype = "dashed", color = "grey")
+    } else {
+      smooth_var <- "vpd"
+      df_long <- as_tibble(curve_mat[, cols_to_plot]) %>%
+        mutate(L = newdat$L) %>%
+        pivot_longer(-L, names_to = "sim", values_to = "partial")
+
+      # median curve across refits
+      median_curve <- apply(curve_mat, 1, mean, na.rm = TRUE)
+      median_df <- tibble(L = newdat$L, fit = median_curve, quartile = q)
+       plot <- ggplot(df_long, aes(x = L, y = partial, group = sim)) +
+          geom_line(alpha = 0.12, color = "black") +
+          geom_line(data = median_df, aes(x = L, y = fit), color = "firebrick", linewidth = 1.1, inherit.aes = FALSE) +
+          scale_x_reverse() +
+          labs(x = "Lag", y = "Partial effect", title = "Refit subsample curves + median") +
+          theme_bw() +  geom_hline(yintercept = 0, linetype = "dashed", color = "grey")
+
+    }
+ 
+    return(list(curves = df_long, median = median_df, smooth_var = smooth_var, plot = plot))
+}
+
+
